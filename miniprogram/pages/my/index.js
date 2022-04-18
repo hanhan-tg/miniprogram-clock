8ea6421ea9fafe7120e4570146def9ba52831560
hanhan <1551547222@qq.com>
2022-04-18 19:37:51 +0800

    feat: add 联系客服

--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -1,10 +1,10 @@
 const { getMyInfo } = require("../../service/index")
-
 // pages/my/index.js
 Page({
 
   data: {
-    info: {}
+    info: {},
+    popupVisible: '',
   },
 
   onClickGroupCreated() {
@@ -13,14 +13,14 @@ Page({
     })
   },
   onClickGroupJoined() {
-    
+
   },
   onClickClock() {
     wx.navigateTo({
       url: '/pages/completeTasks/index',
     })
   },
-  onClickJoin(){
+  onClickJoin() {
     wx.navigateTo({
       url: '/pages/searchGroup/index',
     })
@@ -30,63 +30,28 @@ Page({
       url: '/pages/settings/index',
     })
   },
+  onClickAttach() {
+    this.setData({
+      popupVisible: true
+    })
+  },
+  onPopupVisibleChange({ detail }) {
+    const { visible } = detail;
+    this.setData({
+      popupVisible: visible
+    })
+  },
+  onImageError() {
+    console.log('image error');
+  },
+  onImageLoad() {
+    console.log('image load');
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  },
   onLoad: async function (options) {
     const info = await getMyInfo();
     this.setData({
       info,
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
